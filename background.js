3745ad84302ec94fbeb0e7e348bd8fd479a7d18b
highjeans <highjeans11@hotmail.com>
2022-06-16 22:34:22 -0700

    Implement sending text from background.js

--- background.js
+++ background.js
@@ -3,6 +3,13 @@ chrome.runtime.onInstalled.addListener(async () => {
         id: "notes_add",
         title: "Add Selection to Notes",
         type: "normal",
-        contexts: ["selection"]
+        contexts: ["selection", "audio", "image", "video"]
     })
+})
+
+chrome.contextMenus.onClicked.addListener((info) => {
+    switch (info.mediaType) {
+        case undefined: //TODO Add image, video, and audio cases
+            chrome.runtime.sendMessage({add: info.selectionText}, (response) => {});
+    }
 })
